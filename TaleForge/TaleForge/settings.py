c3dd26203546c08d95c3a9888cc5e31a9a8e6065
Ekansh Goenka <goenka.ekansh@gmail.com>
2023-12-16 12:49:36 +0800

    started django project

--- TaleForge/TaleForge/settings.py
+++ TaleForge/TaleForge/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*t00f50+c^iy=6p-k$xh-60l#g1gwj-yn_#jjn*9@i1sm=8for'
+SECRET_KEY = 'django-insecure-$40b!#lywj^!hgks6$)*pq9pdg-gprsirjwzczv$@pnr%60plh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
